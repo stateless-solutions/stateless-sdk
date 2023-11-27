2e6750e7c5556b12ed54b8c927893e12d3da3dbb
crisog <ortega.cpp@gmail.com>
2023-11-27 12:07:07 -0400

    feat: providers interactive

--- cli/commands/providers.py
+++ cli/commands/providers.py
@@ -1,4 +1,5 @@
-from typer import Typer, Option, Argument
+from typer import Typer, Option, Argument, prompt
+from typing import Optional
 from rich.console import Console
 from rich.table import Table
 from cli.routes import V1Routes
@@ -12,11 +13,25 @@ providers_app = Typer()
 
 @providers_app.command("create")
 def create_provider(
-    config_file: str = Option(
-        ..., help="The path to a JSON file with the provider creation data."
+    config_file: Optional[str] = Option(
+        None, help="The path to a JSON file with the provider creation data."
     )
 ):
-    provider_create = parse_config_file(config_file, ProviderCreate)
+    if config_file:
+        provider_create = parse_config_file(config_file, ProviderCreate)
+    else:
+        oauth_id = prompt("Enter OAuth2 Unique Identifier")
+        status = prompt("Enter Account Status", default="active")
+        email = prompt("Enter Email Address", default=None)
+        name = prompt("Enter Name", default=None)
+        username = prompt("Enter Username")
+        payment_address = prompt("Enter Payment Address", default=None)
+
+        provider_create = ProviderCreate(
+            oauth_id=oauth_id, status=status, email=email, name=name, 
+            username=username, payment_address=payment_address
+        )
+        
     response = make_request_with_api_key(
         "POST", V1Routes.PROVIDERS, provider_create.model_dump_json()
     )
@@ -30,15 +45,16 @@ def create_provider(
 
 
 @providers_app.command("get")
-def get_provider(
-    provider_id: str = Argument(..., help="The ID of the provider to get.")
-):
+def get_provider(provider_id: Optional[str] = Argument(None, help="The ID of the provider to get.")):
+    if not provider_id:
+        provider_id = prompt("Enter the ID of the provider to get")
+        
     response = make_request_with_api_key("GET", f"{V1Routes.PROVIDERS}/{provider_id}")
 
     json_response = response.json()
 
     if response.status_code == 200:
-        console.print(json_response)  # Display the provider details
+        console.print(json_response)
     else:
         console.print(f"Error getting provider: {json_response['detail']}")
 
@@ -52,18 +68,18 @@ def list_providers():
     table = Table(show_header=True, header_style="green")
     table.add_column("ID")
     table.add_column("Name")
-    # Add more columns as needed
 
     for item in json_response["items"]:
-        table.add_row(item["id"], item["name"])  # Add more columns as needed
+        table.add_row(item["id"], item["name"])
 
     console.print(table)
 
 
 @providers_app.command("delete")
-def delete_provider(
-    provider_id: str = Argument(..., help="The ID of the provider to delete.")
-):
+def delete_provider(provider_id: Optional[str] = Argument(None, help="The ID of the provider to delete.")):
+    if not provider_id:
+        provider_id = prompt("Enter the ID of the provider to delete")
+        
     response = make_request_with_api_key(
         "DELETE", f"{V1Routes.PROVIDERS}/{provider_id}"
     )
