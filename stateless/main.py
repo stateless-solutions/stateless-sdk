996b76990ceea46ffb5d1a829e27d93586321f9b
crisog <ortega.cpp@gmail.com>
2024-01-15 14:32:12 -0400

    version bump

--- stateless/main.py
+++ stateless/main.py
@@ -34,7 +34,7 @@ ascii_art = r"""
  |_____/ \__\__,_|\__\___|_|\___||___/___/  \_____|______|_____|                                                        
 """  # noqa: W291
 
-__version__ = "0.0.7" # Keep this in sync with pyproject.toml
+__version__ = "0.0.8" # Keep this in sync with pyproject.toml
 
 def version_callback(value: bool):
     if value:
