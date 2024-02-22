ee6b0600919e3d40840c24418b1463ed424aa0ef
blockjoe <69400937+blockjoe@users.noreply.github.com>
2024-02-22 12:05:26 -0500

    chore: bump version

--- stateless/main.py
+++ stateless/main.py
@@ -35,7 +35,7 @@ ascii_art = r"""
  |_____/ \__\__,_|\__\___|_|\___||___/___/  \_____|______|_____|                                                        
 """  # noqa: W291
 
-__version__ = "0.0.16"  # Keep this in sync with pyproject.toml
+__version__ = "0.0.17"  # Keep this in sync with pyproject.toml
 
 
 def version_callback(value: bool):
